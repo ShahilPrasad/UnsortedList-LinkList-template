7af18fd92f87ece56cb72dce434e06287d8d90c0
Shahil Prasad <sprasad40@horizon.csueastbay.edu>
2022-07-05 10:54:52 -0700

    es

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,4 +1,19 @@
+//4 steps to create a node
+//1 Create Node
+//2 Init Node (point at the next node, and give it value store inside)
+//3 Set Head (since this is starting from the start, the head will point at it)
+//4 book keeping
 
+//Steps to Deleting unsorted List (works in deleting nth and last nodes)
+//1 Find Item
+//2 prevNode needs to point to currNode-> next; (in other words the previous node is linked to the next node (the node after current node, current node is in the middle right now)
+//3 Delete Current Node
+//4 Book Keeping
+//Regarding Deleting head node
+// Steps 1 nd 2 are the same
+// 3: does an if statement to check if(head == currNode)
+// 4: Deletes current node
+// 5: Book Keeping
 
 
 #include "UnsortedList.h"
@@ -7,12 +22,12 @@ template<class T>
 UnsortedList<T>::UnsortedList() {
     head = nullptr; //null pointer (null must be store in the beginning)
     length = 0; //length
-    currPos = nullptr; //current position
+    currPos = nullptr; //current position, currpos used for iterator
 }
 
 template<class T>
 UnsortedList<T>::~UnsortedList() {
-    MakeEmpty();
+    MakeEmpty(); //all the work is done in the function
 
 }
 
@@ -21,9 +36,9 @@ template<class T>
 void UnsortedList<T>::MakeEmpty() {
 
     while(head != nullptr){
-        Node* tmp = head;
-        head = head-> next;
-        delete tmp; //this is everything for deconstructor
+        Node* tmp = head; //makes a temp pointer
+        head = head-> next; //pointing at the next node to be head
+        delete tmp; //this is everything for deconstructor, it deletes the node temp is on
     }
     length =0;
 }
@@ -31,13 +46,13 @@ void UnsortedList<T>::MakeEmpty() {
 template<class T>
 bool UnsortedList<T>::IsFull() const {
     try{
-        Node* tmp = new Node; //trying this code, if it fails it catch
+        Node* tmp = new Node; //try to create a node
         delete tmp;
     }
-    Catch(){
-        return true;
+    catch(...){
+        return true; //if it fails it'll come here and we return true
     }
-    return false;
+    return length;
 }
 
 template<class T>
@@ -47,10 +62,10 @@ int UnsortedList<T>::GetLength() const {
 
 template<class T>
 bool UnsortedList<T>::Contains(T someItem) {
-    Node* currNode = head;
-
-    while(currNode->next != nullptr){ //if the list is empty it will return false, else it would do what's in the loop
-        if(currNode->value == someItem){
+    Node* currNode = head; //points the current node at the head
+//nullptr is the end of the list, this loop will go until it reaches null
+    while(currNode->next != nullptr) { //This loops through the entire list, (in more detail this is saying loop until the next item is null)else it would do what's in the loop
+        if(currNode->value == someItem){ //check if the current node contains that items
             return true;
         }
         currNode = currNode -> next; //move to next
@@ -63,9 +78,9 @@ void UnsortedList<T>::AddItem(T item) { //adds new node at the start (not includ
    if (IsFull())
        return;
 
-    Node* nodeToAdd = new Node; //creates the node
+    Node* nodeToAdd = new Node; //creates the node (immediately set up the value and pointer (next 2 lines)
     nodeToAdd -> value = item; //makes the value for the nodes and set the item value (item/address)
-    nodeToAdd -> next = head; //points at the next node
+    nodeToAdd -> next = head; //new node points at the next node
     head = nodeToAdd; //makes the head point at this node
     length++; //increments the length
 }
@@ -76,14 +91,20 @@ void UnsortedList<T>::DeleteItem(T item) {
     Node* prevNode = nullptr;
 
     while(currNode->next != nullptr){ //if the list is empty it will return false, else it would do what's in the loop
-        if(currNode->value == item){
-            prevNode -> next = currNode -> next; //skips the node we want to delete
+        if(currNode->value == item){  //check if the current node contains that item
+            if(head == currNode){//if head head has this item do this
+                head = currNode -> next; //if the item is stored in head, the next item in the list will be the new head
+            }
+            else{//the if statement nested in this already found it, so if it's not in the head it's in the other nodes
+                prevNode -> next = currNode -> next; //skips the node we want to delete
+            }
+
             delete currNode; //delete the node
             length--;
             return; //it's saying stop running the loop right here
         }
-        prevNode = currNode;
-        currNode = currNode -> next; //move to next
+        prevNode = currNode; //works the same as curnode, helps paint better pic
+        currNode = currNode -> next; //move to next (always 1 ahead of prevNode)
     }
 }
 
